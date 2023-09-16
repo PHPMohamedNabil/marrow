17565fb87ce365b2e00580e160ebeccf25c2ba28
PHPMohamedNabil <mohamedn085@gmail.com>
2023-09-17 01:33:57 +0300

    Update TimeZoneStartup.php

--- app/startups/TimeZoneStartup.php
+++ app/startups/TimeZoneStartup.php
@@ -4,10 +4,10 @@ namespace App\Startups;
 
 use App\Repositories\BookInterface;
 use App\Repositories\BookRepository;
-use App\Core\Container\Container;
+use Core\Container\Container;
 use App\Startups\StartupInterface;
-use App\Core\Database\NativeDB;
-use App\Core\Request;
+use Core\Database\NativeDB;
+use Core\Request;
 class TimeZoneStartup implements StartupInterface
 {
    //timezone service provider if you are saving timezone in db
